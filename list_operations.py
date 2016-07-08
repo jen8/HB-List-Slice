bd42d2194aac9e2ff5350a3ecf1b8cf11da8f781
Hackbright Student <info@hackbrightacademy.com>
2016-07-08 12:05:03 -0700

    almost done

--- list_operations.py
+++ list_operations.py
@@ -133,7 +133,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6:]
 
 
 def inner_four_end(input_list):
@@ -148,7 +148,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2:1]
 
 
 def replace_head(input_list):
@@ -165,7 +165,7 @@ def replace_head(input_list):
 
     """
 
-    pass
+    input_list[0] = 42
 
 
 def replace_third_and_last(input_list):
@@ -182,7 +182,8 @@ def replace_third_and_last(input_list):
 
     """
 
-    pass
+    input_list[2] = 37
+    input_list[-1] = 37
 
 
 def replace_middle(input_list):
@@ -199,7 +200,7 @@ def replace_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2:1] = [42,37]
 
 
 def delete_third_and_seventh(input_list):
